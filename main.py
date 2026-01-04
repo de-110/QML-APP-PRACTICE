2ce2fc3e013b1152a80250e71048222097461d3f
de-110 <183182702+de-110@users.noreply.github.com>
2026-01-04 14:54:11 +0800

    add now use appViewModel.py to communicate with the viewModels

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import os
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
-from ui.viewModels.loginViewModel import LoginViewModel
+from app.appViewModel import AppViewModel
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     
-    login_vm = LoginViewModel()
-    engine.setInitialProperties({"loginViewModel": login_vm})
+    app_vm = AppViewModel(parent=app)
+    engine.rootContext().setContextProperty("ems_connect", app_vm)
     engine.load(os.path.join(os.path.dirname(__file__), "ui/views/login_ui.qml"))
-
-
 
     if not engine.rootObjects():
         sys.exit(-1)
