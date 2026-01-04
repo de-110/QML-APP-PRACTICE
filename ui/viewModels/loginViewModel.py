49db291444161790a10dd9a5d063a5f0a88b6d0f
de-110 <183182702+de-110@users.noreply.github.com>
2026-01-04 14:55:41 +0800

    Refactor login logic and improve input sanitization
    - Rename 'login' slot to 'validateCredentials' for clarity.
    - sanitize inputs by stripping whitespace before verification.
    - Replace 'status_message' (string) with 'login_status' (boolean) to simplify QML color bindings.

--- ui/viewModels/loginViewModel.py
+++ ui/viewModels/loginViewModel.py
@@ -1,6 +1,26 @@
-from PySide6.QtCore import QObject, Slot
+from PySide6.QtCore import QObject, Slot, Signal, Property
 
 class LoginViewModel(QObject):
+    statusChanged = Signal()
+    loginSuccess = Signal()
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.login_status: bool = True
+    
+    @Property(bool, notify=statusChanged)
+    def loginStatus(self):
+        return self.login_status
+
     @Slot(str, str)
-    def login(self, username, password):
-        print("USERNAME: "+ username + "PASSWORD: " + password)
+    def validateCredentials(self, username, password):
+        val_username = username.strip()
+        val_password = password.strip()
+        
+        if val_username == "admin" and val_password == "admin":
+            self.login_status = True
+            self.statusChanged.emit()
+            self.loginSuccess.emit()
+        else:
+            self.login_status = False
+            self.statusChanged.emit()
